cf24601492ed8e8a6e0703d8f3459681d544d08b
Gabriel <gbiren@ucla.edu>
2018-11-05 22:49:33 -0800

    Added .docx support, and printer command (currently commented out)

--- printer_client/printer_client.py
+++ printer_client/printer_client.py
@@ -8,23 +8,26 @@ url = 'http://printzz.herokuapp.com/'
 refresh_time = 2
 print_time = 30
 
-def main():
-    # Delete Any Existing Print Files
-    subprocess.call('rm -f print*', shell=True)
-    
+def poll_server():    
     # Check if There is a File Ready to Print
+    settings_obj = requests.get(url+'get_doc_settings')
+    settings = settings_obj.json()
+    
+    # Return to Loop if No File Exists
+    if (settings['status'] == False):
+        return
+
+    # File Exists, So Download the Actual File
     doc_obj = urllib.request.urlopen(url+'get_doc')
     doc = doc_obj.read()
-    
-    # Restart Loop if No File Exists
-    if ("{\"status\":false}" in doc.decode('ascii')):
-        exit()
 
     # Check For Common File Headers
     extension = ""
-    if ("%PDF-" in doc.splitlines()[0].decode('ascii')):   # PDF
+    if ( (doc[0] == 80) and (doc[1] == 75) ):                # DOCX
+        extension = "docx"
+    elif ("%PDF-" in doc.splitlines()[0].decode('ascii')):   # PDF
         extension = "pdf"
-    else:                                                  # TXT
+    else:                                                    # TXT
         extension = "txt"
 
     # Write File to Disk
@@ -32,8 +35,33 @@ def main():
     f.write(doc)
     f.close
 
-    # Send to the Printer
+    # TODO Check If .docx can be printed directly, or need to convert
+
+    # Generate Print Command Based on Provided Options
+    print_cmd = "lp print." + extension + " -n "
+    print_cmd += str(settings['data']['copies']) + " "      # Number of Copies
+    if (settings['data']['double_sided']):                  # Double-Sided
+        print_cmd += "-o sides=two-sided-long-edge "
+    #TODO Need Printer-Specific Option for Setting Color
     
+    # Issue Print Command to the System
+    #subprocess.call(print_cmd, shell=True)
+    print (print_cmd)
+
+    # Perform Cleanup
+    #subprocess.call('rm -f print.*', shell=True)
+    #requests.get(url+'pop_doc')
+    
+def main():
+    # Delete Any Existing Print Files
+    subprocess.call('rm -f print.*', shell=True)
+
+    # Continuously Poll the Server, Printing Any Files Found
+    while (True):
+        poll_server()
+        time.sleep(refresh_time)
+
 
 if __name__ == "__main__":
-    main()
+    #main()
+    poll_server()
