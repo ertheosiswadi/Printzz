455ac963e1eee40544742b2ed04b9a542c8fbeab
dpadlipsky <david.padlipsky@gmail.com>
2018-11-18 23:45:34 -0800

    printer_auth: add printer authentication
    Adds authentication to printer_status updating, pop doc, get doc, and get doc settings

--- server/src/constants.py
+++ server/src/constants.py
@@ -1,10 +1,11 @@
 USER_DB_FILE = 'users.db'
-QUEUE_FILE = 'queue.db'
+QUEUE_FILE = 'printer.db'
 
 # Constants for generic API Responses
 STATUS_KEY = 'status'
 DATA_KEY = 'data'
 INPUT_FILE_KEY = 'input_file'
+ERROR_KEY = 'error'
 
 USERNAME_KEY = 'username'
 PASSWORD_KEY = 'password'
@@ -16,11 +17,13 @@ PROGRESS_KEY = 'progress'
 SETTINGS_KEY = 'settings'
 PRINTER_STATUS_KEY = 'status'
 
+PRINTER_ID = 'e19e5d74-6ed2-41b6-ad21-ed1c8e7be7e1'
+PRINTER_ID_KEY = 'printer_id'
+
 # Keys for settings fields
 DOUBLE_SIDED_KEY = 'double_sided'
 COLOR_KEY = 'color'
 COPIES_KEY = 'copies'
-
 
 FILES_PATH = 'files'
 DATABASES_PATH = 'databases'
